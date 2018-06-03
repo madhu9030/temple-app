4fa0113dcff1b89962baef0eb8f1aa5239e9191a
madhu <msreddy490@gmail.com>
2018-06-03 02:44:37 -0700

    added basic navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class NavBar extends Component {
     render() {
         return (
-            <div>
-                <button className="button">Hello</button>
-            </div>
+            <nav className="tp-nav-bar-container">
+                <div className="tp-nav-bar-wrapper">
+                    <div className="tp-na-bar-links">
+                        <ul>
+                            <li className="tp-nav-list"><a href="">Home</a></li>
+                            <li className="tp-nav-list"><a href="">About Us</a></li>
+                            <li className="tp-nav-list"><a href="">Services</a></li>
+                            <li className="tp-nav-list"><a href="">Events</a></li>
+                            <li className="tp-nav-list"><a href="">Contact</a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
